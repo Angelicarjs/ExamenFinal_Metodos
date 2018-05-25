afd912ee5bda75ebc2f33ec1c4e81bc3ae0e4834
angelicarjs-examenfinal_metodos-ybv5qbfp <angelicarjs-examenfinal_metodos-ybv5qbfp@local>
2018-05-25 01:05:09 +0000

    grafica ok

--- ejercicio3.py
+++ ejercicio3.py
@@ -4,6 +4,21 @@
 # Calcule la covarianza entre `u` y `v` e imprima su valor.
 
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
 t = np.array([0.,0.1,0.2,0.3,0.4,0.5,0.6, 0.8, 0.9])
 u = np.array([12.,45.,6.,78.,34.,22.,-10.,31.,-27.])
 v = np.array([3.,11.,1.3,37.,11.,6.,-23.,7.,7.])
+
+#Grafica de las series 
+plt.figure()
+plt.scatter(t,v)
+plt.scatter(t,u)
+plt.savefig('serie.png')
+
+#Covarianza
+
+for i in range()
+
