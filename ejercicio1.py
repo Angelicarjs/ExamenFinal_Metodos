667ab639dcbe6a59c5ea98cf72c8829607b1da89
angelicarjs-examenfinal_metodos-ybv5qbfp <angelicarjs-examenfinal_metodos-ybv5qbfp@local>
2018-05-25 01:01:52 +0000

    terminado

--- ejercicio1.py
+++ ejercicio1.py
@@ -8,5 +8,9 @@ import numpy as np
 x = np.linspace(0,2.,10)
 fx = np.array([0., 0.0494, 0.1975, 0.4444, 0.7901,1.2346 , 1.7778, 2.4198, 3.1605, 4.])
 
+#fx[i+1] - fx[i] / x[i+1] - x[i] si fuera con un ciclo 
 
+der1 = fx[1:] - fx[:-1] / (x[1:] - x[:-1]) # primera derivada usando el punto de atras se pierde el primer punto
+
+der2 = der1[1:] - der1[:-1] / (x[2:] - x[:-2]) #segunda derivada usando el punto de atras se pierde los dos primeros puntos
 
